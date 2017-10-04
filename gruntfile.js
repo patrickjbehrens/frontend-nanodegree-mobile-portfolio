69ec08194dd01d39a78eb5df22f6af637d6405c5
Patrick Behrens <patrickjbehrens@gmail.com>
2017-10-04 11:43:09 -0700

    Images resized with grunt responsive-images

--- gruntfile.js
+++ gruntfile.js
@@ -3,25 +3,36 @@ module.exports = function(grunt) {
 
     // All configuration goes here
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+        //pkg: grunt.file.readJSON('package.json'),
 
-        imagemin: {
-            dynamic: {
-                files: [{
-                    expand: true,
-                    cwd: 'img/',
-                    src: ['**/*.{png,jpg,gif}'],
-                    dest: 'img/build/'
-                }]
-            }
-        }
+        responsive_images: {
+          myTask: {
+            options: {
+              engine: 'im',
+              sizes: [{
+                name: 'small',
+                width: 100
+              },{
+                name: 'large',
+                width: 1000,
+                quality: 60
+              }]
+            },
+            files: [{
+              expand: true,
+              src: ['*.{gif,jpg,png}'],
+              cwd: 'views/images/',
+              dest: 'views/images/build/'
+            }]
+          }
+        },
 
     });
 
     // Where we tell Grunt we plan to use this plug-in.
-    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-responsive-images');
 
     // Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['imagemin']);
+    grunt.registerTask('default', ['responsive_images']);
 
 };
